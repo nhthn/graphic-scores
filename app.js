777ecc68ba44a7adb30aaa00e556dc89baed23c0
Nathan Ho <Nathan Ho>
2023-01-20 00:58:35 -0800

    Add resistors

--- app.js
+++ app.js
@@ -29,6 +29,22 @@ class RNG {
     choose(array) {
         return array[this.integer(0, array.length)];
     }
+
+    chooseWeighted(array, weights) {
+        const weightsSum = weights.reduce((x, y) => x + y, 0);
+        const normalizedWeights = weights.map((x) => x / weightsSum);
+        let threshold = this.random();
+        let cumulative = 0;
+        let index = 0;
+        for (let weight of normalizedWeights) {
+            cumulative += weight;
+            if (cumulative >= threshold) {
+                return array[index];
+            }
+            index += 1;
+        }
+        return array[array.length - 1];
+    }
 }
 
 function doLineSegmentsIntersect(segment1, segment2) {
@@ -67,10 +83,10 @@ function generatePoissonDiskSamples(rng, count, width, height, radius, margin) {
                 y: margin + rng.random() * (height - 2 * margin) 
             };
             if (points.every((point) => distance(candidate, point) >= radius)) {
+                points.push(candidate);
                 break;
             }
         }
-        points.push(candidate);
     }
     return points;
 }
@@ -118,9 +134,9 @@ function diagonalify(segment) {
     const dy = Math.abs(point2.y - point1.y);
     let pointMiddle;
     if (dx >= dy) {
-        pointMiddle = { x: point1.x + dy * Math.sign(point2.x - point1.x), y: point1.y };
+        pointMiddle = { x: point1.x + dy * Math.sign(point2.x - point1.x), y: point2.y };
     } else {
-        pointMiddle = { x: point1.x, y: point1.y + dx * Math.sign(point2.y - point1.y)};
+        pointMiddle = { x: point2.x, y: point1.y + dx * Math.sign(point2.y - point1.y)};
     }
     return [
         [point1, pointMiddle],
@@ -165,21 +181,115 @@ function pickPath(segment, existingSegments, rng, weights) {
     return null;
 }
 
-function generate(seed) {
+function makeSquiggles(point1, point2) {
+    const length = distance(point1, point2);
+    const approxSquiggleSize = 5;
+    const squiggleDepth = 3;
+    const numSquiggles = Math.ceil(length / approxSquiggleSize);
+    const dx = (point2.x - point1.x) / numSquiggles;
+    const dy = (point2.y - point1.y) / numSquiggles;
+    const orthogonalX = -(point2.y - point1.y) / length * squiggleDepth;
+    const orthogonalY = (point2.x - point1.x) / length * squiggleDepth;
+    const points = [];
+    let i;
+    let x = point1.x;
+    let y = point1.y;
+    for (i = 0; i < numSquiggles; i++) {
+        points.push({ x: x, y: y });
+        x += dx / 4;
+        y += dy / 4;
+        points.push({ x: x + orthogonalX, y: y + orthogonalY });
+        x += dx / 4;
+        y += dy / 4;
+        points.push({ x: x, y: y });
+        x += dx / 4;
+        y += dy / 4;
+        points.push({ x: x - orthogonalX, y: y - orthogonalY });
+        x += dx / 4;
+        y += dy / 4;
+    }
+    points.push(point2);
+    return points;
+}
+
+function interpolate(point1, point2, t) {
+    return {
+        x: point1.x + (point2.x - point1.x) * t,
+        y: point1.y + (point2.y - point1.y) * t
+    };
+}
+
+function makeResistor(segment, rng) {
+    const point1 = segment[0];
+    const point2 = segment[1];
+    const length = distance(point1, point2);
+
+    const squiggleStart = interpolate(point1, point2, rng.uniform(0.1, 0.4));
+    const squiggleEnd = interpolate(point1, point2, rng.uniform(0.6, 0.9));
+
+    let points = [point1];
+    points = points.concat(makeSquiggles(squiggleStart, squiggleEnd));
+    points.push(point2);
+
+    const parts = [];
+    let first = true;
+    for (let point of points) {
+        if (first) {
+            parts.push(`M ${point.x} ${point.y}`)
+        } else {
+            parts.push(`L ${point.x} ${point.y}`)
+        }
+        first = false; 
+    }
+    return parts.join(" ");
+}
+
+function generateSeedString() {
+    const digits = "0123456789";
+    const length = 12;
+    const result = [];
+    let i;
+    for (i = 0; i < length; i++) {
+        result.push(digits[Math.floor(Math.random() * digits.length)]);
+    }
+    return result.join("");
+}
+
+const NODE_TYPES = ["dot", "rectangle", "triangle", "label"];
+const CURVE_TYPES = ["line", "arc", "squiggle"];
+
+function generate(seedString) {
+    const seed = parseInt(seedString, 10);
+
     const rng = new RNG(seed);
-    const width = 1000;
-    const height = 500;
+    /*
+    const width = 1080;
+    const height = 720;
     const pointCount = rng.integer(30, 100);
-    const poissonRadius = 100;
+    */
+    const width = 700;
+    const height = 400;
+    const pointCount = rng.integer(30, 60);
+
+    const poissonRadius = 50;
     const margin = 30;
-    const density = rng.uniform(0.3, 0.8);
+    const density = rng.uniform(0.5, 1.0);
     const dotDensity = rng.random();
-    const arcProbability = rng.choose([rng.random(), 0]);
 
     const pathTypeWeights = {};
     const power = rng.uniform(1, 5);
     for (let pathType of PATH_TYPES) {
         pathTypeWeights[pathType] = Math.pow(rng.random(), power);
+    }
+
+    const nodeTypeWeights = [];
+    for (let nodeType of NODE_TYPES) {
+        nodeTypeWeights.push(Math.pow(rng.uniform(0.1, 1.0), power));
+    }
+
+    const curveTypeWeights = [];
+    for (let curveType of CURVE_TYPES) {
+        curveTypeWeights.push(rng.uniform(0.1, 1.0));
     }
 
     const draw = SVG().addTo("body").size(width, height);
@@ -202,21 +312,29 @@ function generate(seed) {
         if (rng.random() < density) {
             const path = pickPath(pair, segments, rng, pathTypeWeights);
             if (path !== null) {
+                const width = 1.5;
+                const type = rng.chooseWeighted(CURVE_TYPES, curveTypeWeights);
+                const dasharray = type === "squiggle"
+                    ? null
+                    : rng.random() < 0.2 ? rng.choose(["10,5", "5,5"]) : null;
                 for (let segment of path) {
                     const point1 = segment[0];
                     const point2 = segment[1];
                     const rx = Math.abs(point2.x - point1.x);
                     const ry = Math.abs(point2.y - point1.y);
-                    const width = rng.choose([1, 2]);
-                    draw.path(
-                        rng.random() < arcProbability
-                            ? `M ${point1.x} ${point1.y} A ${rx} ${ry} 0 0 0 ${point2.x} ${point2.y}`
-                            : `M ${point1.x} ${point1.y} L ${point2.x} ${point2.y}`
-                    ).fill("none").stroke({
+                    let path;
+                    if (type === "line") {
+                        path = `M ${point1.x} ${point1.y} L ${point2.x} ${point2.y}`;
+                    } else if (type === "arc") {
+                        path = `M ${point1.x} ${point1.y} A ${rx} ${ry} 0 0 0 ${point2.x} ${point2.y}`;
+                    } else if (type === "squiggle") {
+                        path = makeResistor(segment, rng);
+                    }
+                    draw.path(path).fill("none").stroke({
                         width: width,
                         color: "black",
                         linecap: "round",
-                        dasharray: rng.random() < 0.2 ? rng.choose(["10,5", "5,5"]) : null
+                        dasharray: dasharray
                     });
                     segments.push(segment);
                 }
@@ -226,7 +344,7 @@ function generate(seed) {
 
     for (let point of points) {
         if (rng.random() < dotDensity) {
-            const type = rng.choose(["dot", "rectangle", "triangle"]);
+            const type = rng.chooseWeighted(NODE_TYPES, nodeTypeWeights);
             if (type === "dot") {
                 const radius = rng.choose([3, 5, 10]);
                 const strokeWidth = 2;
@@ -261,6 +379,26 @@ function generate(seed) {
                     .translate(point.x, point.y)
                     .fill("white")
                     .stroke({ width: 1.5, color: "black" });
+            } else if (type === "label") {
+                const width = 25;
+                const height = 21;
+                draw.rect(width, height)
+                    .attr({
+                        x: point.x - width / 2,
+                        y: point.y - height / 2,
+                        rx: 5,
+                        ry: 5
+                    })
+                    .fill("white")
+                    .stroke({ width: 1.5, color: "black" });
+                draw.text(rng.choose(rng.choose(["0123456789", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"])))
+                    .attr({
+                        x: point.x,
+                        y: point.y + height * 0.05,
+                        "font-family": "sans-serif",
+                        "dominant-baseline": "middle",
+                        "text-anchor": "middle"
+                    })
             }
         }
     }
@@ -268,13 +406,22 @@ function generate(seed) {
     return draw;
 }
 
+function updateHash(seedString) {
+    const baseURL = location.href.split("#")[0];
+    location.replace(baseURL + "#" + seedString);
+}
+
 function main() {
-    let draw;
-    draw = generate(0);
-    setInterval(() => {
+    const seedString = location.hash !== ""
+        ? location.hash.substring(1)
+        : generateSeedString();
+    let draw = generate(seedString);
+    document.querySelector("body").addEventListener("click", () => {
         draw.remove();
-        draw = generate(Math.random());
-    }, Math.floor(1.0 * 1000));
+        const seedString = generateSeedString();
+        updateHash(seedString);
+        draw = generate(seedString);
+    });
 }
 
 main();
